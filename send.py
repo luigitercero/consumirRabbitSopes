49de6e8f0fed9528aab47066b7eb14ba2fa06b3a
luigitercero <luigitercero3@gmail.com>
2018-07-02 15:35:48 +0000

    send for rabbit

---
+++ send.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+
+
+credentials = pika.PlainCredentials('admin', '123')
+parameters = pika.ConnectionParameters('35.229.58.120',
+                                       5672,
+                                       '/',
+                                       credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
