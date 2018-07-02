c2df066315c192b8784e37dad417358d769b5e64
luigitercero <luigitercero3@gmail.com>
2018-07-02 11:49:17 -0600

    mensajes

--- consumirRedis.py
+++ consumirRedis.py
@@ -3,6 +3,6 @@ ip_redis = '35.239.110.78'
 redis_entidades = redis.Redis(
     host='35.239.110.78',
     port=6379,
-    password='fVj1HjMcLYXE ',
+    password='fVj1HjMcLYXE',
     db=1)
 redis_entidades.incrby('puta', 1) 
