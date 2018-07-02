5d3226d5f1329d5caaf0e370fc93392253645b94
luigitercero <luigitercero3@gmail.com>
2018-07-02 11:10:07 -0600

    agregando archivo de consumir

---
+++ consumirRedis.py
@@ -0,0 +1,8 @@
+import redis
+ip_redis = '35.239.110.78'
+redis_entidades = redis.Redis(
+    host='35.239.110.78',
+    port=6379,
+    password='fVj1HjMcLYXE ',
+    db=1)
+redis_entidades.incrby('puta', 1) 
